8a24dc559e3e03db915bba23a16e7118f7a08d7e
phamhai040805-lang <phamhai040805@gmail.com>
2025-11-28 22:09:09 +0700

    video 6.1 generate model/migration/seeder

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import configViewEngine from "./config/viewEngine";
 import initWebRoutes from "./routes/web";
 require("dotenv").config();
 import bodyParser from "body-parser";
+// import connection from "./config/connectDB";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -13,6 +14,10 @@ configViewEngine(app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+//test connection DB
+// connection();
+
+//init web routes
 initWebRoutes(app);
 
 app.listen(PORT, () => {
