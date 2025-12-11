15daf2a939b788414e955c43132b400e9869316c
phamhai040805-lang <phamhai040805@gmail.com>
2025-12-11 18:32:25 +0700

    video 8.3 Create Models ORM

--- src/models/groupRole.js
+++ src/models/groupRole.js
@@ -1,7 +1,7 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Project_Role extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -12,20 +12,15 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   //object relational mapping
-  User.init(
+  Project_Role.init(
     {
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      username: DataTypes.STRING,
-      address: DataTypes.STRING,
-      sex: DataTypes.STRING,
-      phone: DataTypes.STRING,
       groupId: DataTypes.INTEGER,
+      roleId: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "Project_Role",
     }
   );
-  return User;
+  return Project_Role;
 };
